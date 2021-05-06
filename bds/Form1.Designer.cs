be8f7bfe4439f6f038ce3b89b89d9a9408ea9a24
Федулова Елена Константиновна <EKFedulova@sberbank.ru>
2021-05-06 21:53:34 +0500

    firs attempt

---
+++ bds/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace bds
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonFrom = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonTo = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxTimer = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-1, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Копировать из:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Копировать в:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(411, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(108, 151);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(411, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // buttonFrom
+            // 
+            this.buttonFrom.Location = new System.Drawing.Point(33, 26);
+            this.buttonFrom.Name = "buttonFrom";
+            this.buttonFrom.Size = new System.Drawing.Size(75, 23);
+            this.buttonFrom.TabIndex = 4;
+            this.buttonFrom.Text = "from";
+            this.buttonFrom.UseVisualStyleBackColor = true;
+            this.buttonFrom.Click += new System.EventHandler(this.buttonFrom_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(558, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(739, 426);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // buttonTo
+            // 
+            this.buttonTo.Location = new System.Drawing.Point(33, 122);
+            this.buttonTo.Name = "buttonTo";
+            this.buttonTo.Size = new System.Drawing.Size(75, 23);
+            this.buttonTo.TabIndex = 6;
+            this.buttonTo.Text = "to";
+            this.buttonTo.UseVisualStyleBackColor = true;
+            this.buttonTo.Click += new System.EventHandler(this.buttonTo_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 415);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 7;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(132, 415);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 8;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Периодичность, сек";
+            // 
+            // textBoxTimer
+            // 
+            this.textBoxTimer.Location = new System.Drawing.Point(187, 253);
+            this.textBoxTimer.Name = "textBoxTimer";
+            this.textBoxTimer.Size = new System.Drawing.Size(85, 22);
+            this.textBoxTimer.TabIndex = 10;
+            this.textBoxTimer.Text = "10";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(117, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(232, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Нажмите кнопку дл выбора папки";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(117, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(240, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Нажмите кнопку для выбора папки";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 308);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Таймер:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(105, 308);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 17);
+            this.label7.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1309, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxTimer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonTo);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonFrom);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonFrom;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonTo;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxTimer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ bds/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SQLite;
+
+namespace bds
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        DataTable dTableTest30001 = new DataTable();
+        DataTable dTableTest30002 = new DataTable();
+
+        SQLiteConnection connection = new SQLiteConnection();
+
+        private SQLiteCommand Test3000Cmd;
+
+        private string startPath; // тут будем хранить путь к папке исмточнику
+        bool startPathFlag = false;
+        private string destPath; // тут будем хранить путь к папке назначения
+        bool destPathFlag = false;
+        bool triger = false;
+        int time, ntime;
+        string nameD, nameM;
+        string nameOldD, nameOldM;
+        string From, To;
+        int countRows = 0;
+        int timerTime = 7;//задать время, на котором будет происходить копирование
+        SQLiteCommand m_sqlCmd = new SQLiteCommand();
+
+        private void buttonFrom_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            // если папка выбрана и нажата клавиша `OK` - значит можно получить путь к папке
+            if (result == DialogResult.OK)
+            {
+                // запишем в нашу переменную путь к папке
+                startPath = folderBrowserDialog1.SelectedPath;
+                startPathFlag = true;
+                richTextBox1.Text += "Выбрана исходная папка" + "\n";
+                label4.Text = startPath;
+            }
+        }
+
+        private void buttonTo_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            // если папка выбрана и нажата клавиша `OK` - значит можно получить путь к папке
+            if (result == DialogResult.OK)
+            {
+                // запишем в нашу переменную путь к папке
+                destPath = folderBrowserDialog1.SelectedPath;
+                destPathFlag = true;
+                richTextBox1.Text += "Выбрана конечная папка" + "\n";
+                label5.Text = destPath;
+            }
+        }       
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (startPathFlag == true && destPathFlag == true)//если указаны папки источник и назначение
+            {
+                Path();//указывается путь к сегодняшней базе
+                if (triger == false)
+                {
+                    time = Convert.ToInt32(textBoxTimer.Text);
+                    ntime = time;
+                    triger = true;
+                }
+                if (triger == true && ntime != Convert.ToInt32(textBoxTimer.Text))//запускает таймер
+                {
+                    time = Convert.ToInt32(textBoxTimer.Text);
+                    ntime = time;
+                }
+
+                try
+                {
+                    textBoxTimer.Enabled = false;
+                    timer1.Start();
+                    buttonStart.Enabled = false;
+                    buttonStop.Enabled = true;
+                    richTextBox1.Text += "Таймер запущен пользователем в: " + DateTime.Now + "\n";
+                }
+                catch
+                {
+                    richTextBox1.Text += "Ошибка в заданном времени таймера в: " + DateTime.Now + "\n";
+                }
+
+
+            }
+
+            else
+            {
+                richTextBox1.Text += "Ошибка указания пути. Укажите корректный путь";
+            }
+            
+        }
+
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            textBoxTimer.Enabled = true;
+            timer1.Stop();
+            buttonStop.Enabled = false;
+            buttonStart.Enabled = true;
+            richTextBox1.Text += "Таймер остановлен пользователем в: " + DateTime.Now + "\n";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time = time - 1;
+            label7.Text = time.ToString()+"сек";
+
+            if (time == timerTime)
+            {
+                richTextBox1.Text += "time" + "\n";
+                Test3000Up();
+            }
+            if (time == 0)
+            {
+                time = Convert.ToInt32(textBoxTimer.Text);
+            }
+        }
+
+        void Test3000Up()
+        {
+            if (!File.Exists(To))
+            {
+                richTextBox1.Text += "Ошибка! База Test3000.sqlite3 сервера не существует или отсутствует по указанному пути" + "\n";
+            }
+            if (File.Exists(To))//(destPath))//ищект максимальную дату в локальной базе(Test3000ServerBasePath), подключается к серверу
+                                //заполняет какую то новую базу dTableTest30001 
+                                //SQLiteDataAdapter???
+                                //вставляет только максимальную дату
+            {
+                connection = new SQLiteConnection(@"Data Source=" + To + ";" + "Version =3; ReadOnly=True;");
+                connection.Open();
+                string command1 = "SELECT  MAX (TIMESTAMP) FROM 'TEST'";
+                SQLiteCommand sqlCommand = new SQLiteCommand(command1);                
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(command1, connection);
+                richTextBox1.Text += "Запрос на поиск последней записи выполнен: \n";
+                adapter.Fill(dTableTest30001);
+                richTextBox1.Text += dTableTest30001.Rows[0][0] + " \n";
+                connection.Close();
+                                
+
+            }
+            if (!File.Exists(From))
+            {
+                richTextBox1.Text += "Ошибка! База Test3000.sqlite3 (исходная) не существует или отсутствует по указанному пути" + "\n";
+            }
+            if (File.Exists(From))//(startPath))//проверяет надичие основной бд
+                                                      //выбирает данные, у которых дата больше последней даты из dTableTest30001
+                                                      //копирует эти данные в dTableTest30002
+                                                      //
+            {
+                richTextBox1.Text += "Есть подключение к основной базе Test3000.sqlite3" + "\n";
+                connection = new SQLiteConnection(@"Data Source=" + From + ";" + "Version =3; ReadOnly=True;");
+                string command2 = "SELECT  * FROM 'TEST' WHERE TIMESTAMP > '" + dTableTest30001.Rows[0][0] + "'";
+                SQLiteCommand sqlCommand = connection.CreateCommand();
+                connection.Open();
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(command2, connection);
+                richTextBox1.Text += "Запрос на поиск расхождений в базах Test3000.sqlite3 выполнен \n";
+                adapter.Fill(dTableTest30002);
+                richTextBox1.Text += "Найдено новых строк: " + dTableTest30002.Rows.Count + "\n";/////////
+                connection.Close();
+            }
+
+            Test3000Cmd = new SQLiteCommand();
+            connection = new SQLiteConnection(string.Format("Data Source={0};", To));
+            if (connection.State != ConnectionState.Open)
+            {
+                connection = new SQLiteConnection("Data Source=" + To + ";Version=3;");
+                connection.Open();
+                richTextBox1.Text += "Серверная база для записи Test3000.sqlite3 успешно открыта \n";
+                Test3000Cmd.Connection = connection;
+            }
+            try
+            {
+                for (int i = 0; i < dTableTest30002.Rows.Count; i++)//из dTableTest30002 бд вставлет все столбцы в основную
+                {
+                    Test3000Cmd.CommandText = "INSERT INTO 'TEST' (TIMESTAMP, OPERATOR, NAME, MAINCOUNTER, RESULT, END, MARK) VALUES('"
+                    + dTableTest30002.Rows[i][0] + "','" + dTableTest30002.Rows[i][1] + "','" + dTableTest30002.Rows[i][2] + "','" + dTableTest30002.Rows[i][3] + "','" + dTableTest30002.Rows[i][4] + "','" + dTableTest30002.Rows[i][5] +
+                    "','" + null + "','" + null + "')" + "\n";
+                    Test3000Cmd.ExecuteNonQuery();
+                }
+                richTextBox1.Text += "Базы Test3000.sqlite3 полностью синхронизированы в " + DateTime.Now + "\n";
+                richTextBox1.Text += "\n";
+            }
+            catch (SQLiteException ex)
+            {
+                richTextBox1.Text += "Ошибка синхронизации баз Test3000.sqlite3 " + ex.Message + DateTime.Now + "\n";
+            }
+            connection.Close();
+        }
+
+        void Path()//меняет формат времени, указывает путь
+        {
+            DateTime YYMMDD = DateTime.Now;
+            if (Convert.ToInt32(DateTime.Parse(YYMMDD.ToString()).ToString("MM")) < 10)//если месяц, допустм, 8, то пишет не 08, а 8
+            {
+                nameM = DateTime.Parse(YYMMDD.ToString()).ToString("MM").Substring(1);
+            }
+            else
+            {
+                nameM = DateTime.Parse(YYMMDD.ToString()).ToString("MM");
+            }
+
+            if (Convert.ToInt32(DateTime.Parse(YYMMDD.ToString()).ToString("dd")) < 10)//аналогично месяцу
+            {
+                nameD = DateTime.Parse(YYMMDD.ToString()).ToString("dd").Substring(1);
+            }
+            else
+            {
+                nameD = DateTime.Parse(YYMMDD.ToString()).ToString("dd");
+            }
+
+
+            textBox1.Text = startPath + @"\" + nameM + "." + DateTime.Parse(YYMMDD.ToString()).ToString("yyyy") + @"\" + nameD + "." + nameM + "." + DateTime.Parse(YYMMDD.ToString()).ToString("yyyy") + ".db";//ищет базу данных этой даты - указывает путь
+                                                                                                                                                                                                                // textBox1.Text = startPath + @"\" + nameD + "." + nameM + "." + DateTime.Parse(YYMMDD.ToString()).ToString("yyyy") + ".db";
+             /////////////////////////////////////                                                                                                                                                                                                   /////////////////////////
+            ///какое должно быть расширение? sqlite3 или db???????????????
+            //////////////////////////////
+            textBox2.Text = destPath + @"\" + nameM + "." + DateTime.Parse(YYMMDD.ToString()).ToString("yyyy") + @"\" + nameD + "." + nameM + "." + DateTime.Parse(YYMMDD.ToString()).ToString("yyyy") + ".sqlite3";
+            //textBox2.Text = destPath + @"\" + nameD + "." + nameM + "." + DateTime.Parse(YYMMDD.ToString()).ToString("yyyy") + ".db";
+
+            From = textBox1.Text;//файл источник
+            To = textBox2.Text;//файл назначения
+        }
+    }
+}
